1025b576dc007b0e73d1299835d8b5991197c960
Oren Mittman <nihil2501@gmail.com>
2022-10-28 19:13:07 -0400

    fix linkforming bug

--- lib/modul_dot_io/system/link_forming.ex
+++ lib/modul_dot_io/system/link_forming.ex
@@ -3,40 +3,32 @@ defmodule ModulDotIo.System.LinkForming do
 
   alias ModulDotIo.System.{Io, Link, Patch}
 
-  def start, do: Agent.start(fn -> [] end, name: __MODULE__)
+  def start, do: Agent.start(fn -> %MapSet{} end, name: __MODULE__)
 
-  def deselect_io(%Io{direction: :output} = io), do: unset_output_io(io)
+  def deselect_io(%Io{direction: :output} = io), do: remove_output_io(io)
   def deselect_io(%Io{direction: :input}) do end
 
-  def select_io(%Io{direction: :output} = io), do: set_output_io(io)
+  def select_io(%Io{direction: :output} = io), do: add_output_io(io)
   def select_io(%Io{direction: :input} = input_io) do
-    with %Io{} = output_io <- get_output_io() do
+    with [%Io{} = output_io] <- get_output_ios() do
       link = %Link{input_io: input_io, output_io: output_io}
       Patch.toggle_link(link)
     end
   end
 
-  defp unset_output_io(io) do
-    get_and_update_output_io(fn current_io ->
-      if(io == current_io, do: :pop, else: {current_io, current_io})
-    end)
+  defp remove_output_io(io) do
+    change_output_ios(&MapSet.delete(&1, io))
   end
 
-  defp set_output_io(io) do
-    get_and_update_output_io(fn current_io ->
-      if(current_io, do: {current_io, current_io}, else: {current_io, io})
-    end)
+  defp add_output_io(io) do
+    change_output_ios(&MapSet.put(&1, io))
   end
 
-  defp get_output_io do
-    get = &Keyword.get(&1, :output_io)
-    Agent.get(__MODULE__, get)
+  defp change_output_ios(change) do
+    Agent.update(__MODULE__, change)
   end
 
-  defp get_and_update_output_io(fun) do
-    Agent.update(__MODULE__, fn state ->
-      {_, next_state} = Keyword.get_and_update(state, :output_io, fun)
-      next_state
-    end)
+  defp get_output_ios do
+    Agent.get(__MODULE__, &MapSet.to_list/1)
   end
 end
